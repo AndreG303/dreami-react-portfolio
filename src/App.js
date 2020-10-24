d975b45dd0495c98f920133932382323371a4819
Andrea Gamez <andreamgamez@gmail.com>
2020-10-24 09:19:47 -0700

    fixed style.css connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 import './css/body.css';
+
 import Home from './pages/home';
 import Portfolio from "./pages/Portfolio"
 import Contact from "./pages/contact"
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,16 @@
 /* eslint-disable jsx-a11y/anchor-has-content */
-import React from 'react';
+import React, {Fragment} from 'react';
 import { Link } from 'react-router-dom';
-import '../css/body.css';
+
 
 function Home() {
     return (
-        <>
-            <header classNameName="jumbotron jumbotron-fluid">
+        <Fragment>
+            <header className="jumbotron jumbotron-fluid">
                 <div className="container">
                     <h1 className="display-4">...an<span className="dreami">DREAMi</span>lena...</h1>
                     <p className="lead">Ready to win...Win Win Win</p>
-                    <a  href="mailto:andreamgamez@gmail.com" className="btn btn-dark"><i className="fa fa-envelope"
+                    <a href="mailto:andreamgamez@gmail.com" className="btn btn-dark"><i className="fa fa-envelope"
                         aria-hidden="true"></i></a>
                     <a href="https://github.com/AndreG303" className="btn btn-dark"><i className="fa fa-github"
                         aria-hidden="true"></i></a>
@@ -27,7 +27,7 @@ function Home() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <Link  className="nav-link" to="/">About</Link>
+                            <Link className="nav-link" to="/">About</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/portfolio">Portfolio</Link>
@@ -39,31 +39,114 @@ function Home() {
                 </div>
             </nav>
             <section>
-                <div className="container card mb-3" style={{maxWidth: "540px"}}>
+                <div className="container card mb-3" style={{ maxWidth: "540px" }}>
                     <div className="row no-gutters">
                         <div className="col-md-3">
-                            <img src="assets/images/IMG_3096.jpg" className="card-img img-fluid" alt="selfie"/>
-            </div>
-                            <div className="col-md-8">
-                                <div className="card-body">
-                                    <h3 className="card-title">ANDREA M GAMEZ</h3>
-                                    <p className="card-text">Bilingual Frontend Web Developer with a broad customer service background.
-                                    Problem solver, versatile, organized and detail-oriented. Skilled in HTML, CSS, JavaScript
-                        and JQuery.</p>
-                                    <a href="assets/Resume.pdf" className="btn btn-dark">RESUME</a>
-                                </div>
+                            <img src="assets/images/IMG_3096.jpg" className="card-img img-fluid" alt="selfie" />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h3 className="card-title">ANDREA M GAMEZ</h3>
+                                <p className="card-text">Creative Full Stack Web Developer with a broad customer service background.
+                                Bilingual, problem solver, versatile, organized and detail-oriented. Skilled in HTML, CSS, Bootstrap, JavaScript, JQuery, Node.js, React amoung others.</p>
+                                <a href="./assets/Resume.pdf" className="btn btn-dark">RESUME</a>
                             </div>
                         </div>
                     </div>
-</section>
-                <footer className="footer">
-                    <nav
-                        className="navbar fixed-bottom navbar-light bg-transparent flex-fill justify-content-center padding-top: 60px;">
-                        <span className="navbar-text">
-                            win win win win <span role="img"aria-label="Heart">❤️</span> &COPY; andreaMgamez </span>
-                    </nav>
-                </footer>
-</>
+                </div>
+            </section>
+            <main class="container">
+                <div class="row">
+                    <div class="col-sm-4 py-2">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h3 class="card-title">STELLAR SONIC</h3>
+                                <img src="assets/images/Stellar sonic.png" class="card-img-top" alt="pasword generator" />
+                                <p class="card-text">A place to share your sound!(project 2)</p>
+                                <a href="https://dry-shelf-60979.herokuapp.com/" class="btn btn-primary">Check it
+                            out</a>
+                                <a href="https://github.com/AndreG303/Stellar-Sonic" class="btn btn-primary">Github
+                            Repository</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 py-2">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h3 class="card-title">EMPLOYEE TEMPLATE GENERATOR</h3>
+                                <img src="assets/images/Screen Shot 2020-08-11 at 7.13.40 PM copy.png" class="card-img-top" alt="Smokey game" />
+                                <p class="card-text">A software engineering team generator command line application</p>
+                                <a href="https://scratch.mit.edu/projects/396064541" class="btn btn-primary">check it out</a>
+                                <a href="https://github.com/AndreG303/Employee-Template-Generator-" class="btn btn-primary">Github
+                            Repository</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 py-2">
+                        <div class="card h-100">
+                            <div class="card-body">
+                                <h3 class="card-title">WEATHER DASHBOARD</h3>
+                                <img src="assets/images/weather.png" class="card-img-top" alt="Weather-Dashboard-AG"/>
+                                    <p class="card-text">A Weather Dashboard application retrieving data from a third party API
+                            (openWeather API), using HTML, CSS and Jquery.</p>
+                                    <a href="https://andreg303.github.io/Weather-Dashboard-AG/index.html"
+                                        class="btn btn-primary">Check it out</a>
+                                    <a href="https://github.com/AndreG303/Weather-Dashboard-AG" class="btn btn-primary">Github
+                            Repository</a>
+                    </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 py-2">
+                            <div class="card h-100">
+                                <div class="card-body">
+                                    <h3 class="card-title">SUPER QUIZ GAMEZ</h3>
+                                    <img src="assets/images/quiz.png" class="card-img-top" alt="superquizgamez"/>
+                                        <p class="card-text">A responsive timed code and trivia quiz with multiple-choice questions and
+                            interactive challenges.</p>
+                                        <a href=" https://andreg303.github.io/superquizgamez/" class="btn btn-primary">Play</a>
+                                        <a href="https://github.com/AndreG303/superquizgamez" class="btn btn-primary">Github
+                            Repository</a>
+                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-4 py-2">
+                                <div class="card h-100">
+                                    <div class="card-body">
+                                        <h3 class="card-title">WORK DAY PLANNER</h3>
+                                        <img src="assets/images/calendar.png" class="card-img-top" alt="work day planner"/>
+                                            <p class="card-text">A simple calendar application that allows the user to save events for each
+                            hour of the day.</p>
+                                            <a href="https://andreg303.github.io/WorkDaySchedulerAG/" class="btn btn-primary">Check it
+                            out</a>
+                                            <a href="https://github.com/AndreG303/WorkDaySchedulerAG" class="btn btn-primary">Github
+                            Repository</a>
+                    </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-4 py-2">
+                                    <div class="card h-100">
+                                        <div class="card-body">
+                                            <h3 class="card-title">VIRTUAL ART GALLERY</h3>
+                                            <img src="assets/images/gallery.png" class="card-img-top" alt="Virtual_Art_Gallery"/>
+                                                <p class="card-text">Dynamic gallery App that can search for pieces of fantasy artwork based on
+                            the user input or at random.</p>
+                                                <a href="https://nlamonaco86.github.io/Virtual_Art_Gallery/" class="btn btn-primary">Check it
+                            out</a>
+                                                <a href="https://github.com/AndreG303/Virtual_Art_Gallery" class="btn btn-primary">Github
+                            Repository</a>
+                    </div>
+                                        </div>
+                                    </div>
+                                </div>
+            </main>
+                            <footer className="footer">
+                                <nav
+                                    className="navbar fixed-bottom navbar-light bg-transparent flex-fill justify-content-center padding-top: 60px;">
+                                    <span className="navbar-text">
+                                        win win win win <span role="img" aria-label="Heart">❤️</span> &COPY; andreaMgamez </span>
+                                </nav>
+                            </footer>
+</Fragment>
 )
 }
 export default Home; 
