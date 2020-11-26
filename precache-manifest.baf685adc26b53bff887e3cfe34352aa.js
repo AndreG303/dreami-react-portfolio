63edab32d7a4a9bee2681c5e1184bcd5e95d8801
Andrea Gamez <andreamgamez@gmail.com>
2020-11-26 09:51:38 -0800

    Updates

---
+++ precache-manifest.baf685adc26b53bff887e3cfe34352aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e33dfa3f0fd2202e16ce4c2d7190b53",
+    "url": "/dreami-react-portfolio/index.html"
+  },
+  {
+    "revision": "515e597e3d7ae9e6ba09",
+    "url": "/dreami-react-portfolio/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "da54ee37b220efe26a26",
+    "url": "/dreami-react-portfolio/static/css/main.49ba8448.chunk.css"
+  },
+  {
+    "revision": "515e597e3d7ae9e6ba09",
+    "url": "/dreami-react-portfolio/static/js/2.cd18b4e7.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/dreami-react-portfolio/static/js/2.cd18b4e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da54ee37b220efe26a26",
+    "url": "/dreami-react-portfolio/static/js/main.77d977b7.chunk.js"
+  },
+  {
+    "revision": "f777bce79b87a8930603",
+    "url": "/dreami-react-portfolio/static/js/runtime-main.c5a1eb7b.js"
+  }
+]);
